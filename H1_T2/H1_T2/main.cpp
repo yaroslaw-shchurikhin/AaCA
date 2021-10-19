2ff26244d091b0555ad10e3d34d45e1ba875ea9c
yaroslaw-shchurikhin <yaroslav.shurihin@gmail.com>
2021-10-19 16:24:30 +0300

    Hot fix T2 H1

--- H1_T2/H1_T2/main.cpp
+++ H1_T2/H1_T2/main.cpp
@@ -3,22 +3,26 @@
 #include <utility>
 
 template <typename T>
-std::pair<bool, std::pair<int, T>> haveMajority(std::vector<T>& data);
+using majority_pair = std::pair<bool, std::pair<unsigned, T>>;
+
+template <typename T>
+majority_pair<T> haveMajority(std::vector<T>& data);
 
 int main()
 {
-  std::vector<int> data = { 1,2,2, 1 };
+  std::vector<int> data = { 1,2 };
 
   if (haveMajority(data).first == true)
   {
     std::cout << "The majority of this array: " << haveMajority(data).second.second;
   }
   else std::cout << "There isn't majority in this array!";
+
   return 0;
 }
 
 template <typename T>
-std::pair<bool, std::pair<int, T>> haveMajority(std::vector<T>& data)
+majority_pair<T> haveMajority(std::vector<T>& data)
 {
   bool result = false;
 
@@ -30,11 +34,12 @@ std::pair<bool, std::pair<int, T>> haveMajority(std::vector<T>& data)
 
   std::vector<T> data_1(data.begin(), data.begin() + data.size() / 2);
   std::vector<T> data_2(data.begin() + data.size() / 2, data.end());
-  std::pair<bool, std::pair<int, T>> m1 = haveMajority<T>(data_1);
+
+  auto m1 = haveMajority<T>(data_1);
 
   if (m1.first)
   {
-    for (auto i = 0; i < data_2.size(); ++i)
+    for (auto i = 0u; i < data_2.size(); ++i)
     {
       if (data_2[i] == m1.second.second) ++(m1.second.first);
     }
@@ -42,11 +47,11 @@ std::pair<bool, std::pair<int, T>> haveMajority(std::vector<T>& data)
     if (m1.second.first > (data.size() + 1) / 2) return m1;
   }
 
-  std::pair<bool, std::pair<int, T>> m2 = haveMajority<T>(data_2);
+  auto m2 = haveMajority<T>(data_2);
 
   if (m2.first)
   {
-    for (auto i = 0; i < data_1.size(); ++i)
+    for (auto i = 0u; i < data_1.size(); ++i)
     {
       if (data_1[i] == m2.second.second) ++(m2.second.first);
     }
